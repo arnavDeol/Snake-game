983c218288315cd8576686d13ae6cfd10538e4f4
arnavDeol <arnavdeol1@gmail.com>
2024-04-15 17:56:29 -0400

    Small formatting changes + readme

--- GamePanel.java
+++ GamePanel.java
@@ -49,11 +49,15 @@ public class GamePanel extends JPanel implements ActionListener {
     public void draw(Graphics g){
 
         if(running) {
-            g.setColor(Color.darkGray);
-            for(int i=0;i<(SCREEN_HEIGHT/UNIT_SIZE);i++){
-                g.drawLine((i*UNIT_SIZE), 0, (i*UNIT_SIZE), SCREEN_HEIGHT);
-                g.drawLine(0,i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
-            }
+
+            // Un-comment if you would like to see gridlines appear
+
+            // g.setColor(Color.darkGray);
+            // for(int i=0;i<(SCREEN_HEIGHT/UNIT_SIZE);i++){
+            //     g.drawLine((i*UNIT_SIZE), 0, (i*UNIT_SIZE), SCREEN_HEIGHT);
+            //     g.drawLine(0,i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+            // }
+
             g.setColor(Color.red);
             g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
@@ -72,14 +76,12 @@ public class GamePanel extends JPanel implements ActionListener {
             g.setFont(new Font("Arial",Font.BOLD,40));
             FontMetrics metrics = getFontMetrics(g.getFont());
             g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: " + applesEaten))/2, g.getFont().getSize());
-            
         } else {
             gameOver(g);
         }
     }
 
     public void move(){
-
         for(int i = bodyParts;i > 0; i--){
             x[i] = x[i-1];
             y[i] = y[i-1];
